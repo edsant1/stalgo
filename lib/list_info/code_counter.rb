7751c7458d4160d72bf865f58c802ca9db3ff2ec
edsant1 <edwins10329@gmail.com>
2014-10-08 17:29:59 -0600

    finished inputing all code for stats

--- lib/list_info/code_counter.rb
+++ lib/list_info/code_counter.rb
@@ -4,6 +4,8 @@ module ListInfo
 		#Count the lines of Ruby code in your app
 		
 		COUNTER = "find . -iname '*.rb' -type f -exec cat {} \\; | wc -l"
+		HAS_MANY_SEARCH = "ack \" has_many \" -c | awk -F \":\" '{print $2,$1}' | grep -v \"0\" | sort -rn"
+		LOC = "find . -iname '*.rb' -type f -exec wc -l {} \\; | sort -rn"
 
     def self.get_raw(command)
       `#{command}`
@@ -13,17 +15,14 @@ module ListInfo
       get_raw(COUNTER).split("\n")
     end
 
-    #Sort the Ruby files in your project by number of has_many
-
-    HAS_MANY_SEARCH = "ack \" has_many \" -c | awk -F \":\" '{print $2,$1}' | grep -v \"0\" | sort -rn"
-
-    def self.get_rw(command)
-      `#{command}`
+  	#Sort the Ruby files in your project by number of has_many
+    def self.get_has_many_relationships()
+      get_raw(HAS_MANY_SEARCH).split("\n")
     end
 
-    def self.get_has_many_relationships
-      get_rw(HAS_MANY_SEARCH).split("\n")
+    #Sort the Ruby files in project by LOC
+    def self.get_sort_loc()
+      get_raw(LOC).split("\n")
     end
-
-  end
+	end
 end
--- stalgo.gemspec
+++ stalgo.gemspec
@@ -22,5 +22,8 @@ Gem::Specification.new do |spec|
 
   # spec.add_development_dependency "bundler", "~> 1.6"
   # spec.add_development_dependency "rake"
-  spec.add_development_dependency "pg"
+  s.add_development_dependency "pg"
+  s.add_development_dependency "rspec-rails", "3.1.0"
+  s.add_development_dependency "pry-rails"
+  s.add_development_dependency "pry-nav"
 end
